3daad5b284494be8aedf24a2826657987df34e35
RanySanto <152874127+RanySanto@users.noreply.github.com>
2026-09-09 23:50:40 -0300

    Update app.py

--- My Opgg/app.py
+++ My Opgg/app.py
@@ -5,7 +5,7 @@ from lol_matches import get_search_player_info
 from lol_matches import get_player
 from waitress import serve
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 @app.route('/')
 @app.route('/index')
